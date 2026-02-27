0b7e166b25289bbc4731e1f82f966d73ccc96611
i0bz <andreiancheta101@gmail.com>
2026-02-28 01:38:46 +0800

    fix: refactor for the cli code to split up the code, been practicing on java swing, am currently creating the prototype ui for now, might take a while since java swing is a cluster ****

--- src/main/ui/cli/AttendanceSystemCLI.java
+++ src/main/ui/cli/AttendanceSystemCLI.java
@@ -13,12 +13,15 @@ public class AttendanceSystemCLI {
     private final AttendanceSystemController systemController;
     private final InputValidator validator;
     private final Scanner stdin;
+    private final List<String> attendanceLists;
 
 
     AttendanceSystemCLI(Scanner stdin, InputValidator validator, AttendanceSystemController systemController) {
         this.stdin = stdin;
         this.systemController = systemController;
         this.validator = validator;
+
+        attendanceLists = this.systemController.attendanceDateLists();
     }
 
 
@@ -29,38 +32,13 @@ public class AttendanceSystemCLI {
     void initCLI() {
         System.out.println("-------Attendance System CLI-------");
 
-        List<String> attendanceLists = systemController.attendanceDateLists();
         int i = 0, decision;
 
-
-        System.out.println();
-        if (attendanceLists.isEmpty()) {
-            System.out.println("There are no registered attendances.");
-            return;
-        }
+        if(hasAttendances()) return;
 
 
 
-        System.out.println("-------Date List-------");
-        for (String attendanceDate : attendanceLists) {
-            System.out.println(++i + ". " + attendanceDate);
-        }
-
-        System.out.println();
-        System.out.print("Select Attendance: ");
-        decision = validator.safeIntInput();
-
-        String date;
-
-        while (true) {
-            try {
-                date = attendanceLists.get(decision - 1);
-                break;
-            } catch (IndexOutOfBoundsException e) {
-                System.out.print("Select valid number: ");
-                decision = validator.safeIntInput();
-            }
-        }
+        AttendanceSheet sheet = attendanceSelector();
 
 
         Map<String, String> rosterList = systemController.rosterLists();
@@ -68,11 +46,9 @@ public class AttendanceSystemCLI {
         System.out.println();
         System.out.println();
 
-        i = 0;
-
         System.out.println("-------Attendance Student List-------");
         for (Map.Entry<String, String> student : rosterList.entrySet()) {
-            System.out.println(++i + ". " + student.getValue() + "\t " + student.getKey() + " " + systemController.isPresent(student.getKey(), date) );
+            System.out.println(++i + ". " + student.getValue() + "\t " + student.getKey() + " " + systemController.isPresent(student.getKey(), sheet.date().toString()) );
         }
 
 
@@ -94,29 +70,44 @@ public class AttendanceSystemCLI {
                 decision = validator.safeIntInput();
             }
         }
-        systemController.toggleAttendance(studentChosen, date);
+        systemController.toggleAttendance(studentChosen, sheet.date().toString());
 
     }
-
-
 
     public void attendanceMode() {
         System.out.println();
         System.out.println("-------Attendance Mode-------");
 
-        List<String> attendanceLists = systemController.attendanceDateLists();
-        int i = 0, decision;
+        if (hasAttendances()) return;
+        AttendanceSheet sheet = attendanceSelector();
 
 
         System.out.println();
-        if (attendanceLists.isEmpty()) {
-            System.out.println("There are no registered attendances.");
-            return;
+        System.out.println("-------Attendance Mode : " + sheet.date() + "-------");
+
+
+        int parsedUid;
+        String line = "";
+        while (true) {
+            try {
+                System.out.println("Enter UID (or q to exit):");
+                line = stdin.nextLine();
+                parsedUid = ParseUtility.parseUID(line);
+                sheet.markPresent(parsedUid);
+            } catch (RuntimeException e) {
+                if (line.equals("q")) return;
+
+                System.out.print("Enter valid uid: ");
+                line = stdin.nextLine();
+            }
         }
 
+    }
 
-
+    private AttendanceSheet attendanceSelector() {
+        int i = 0, decision;
         System.out.println("-------Attendance Date List-------");
+
         for (String attendanceDate : attendanceLists) {
             System.out.println(++i + ". " + attendanceDate);
         }
@@ -137,26 +128,17 @@ public class AttendanceSystemCLI {
             }
         }
 
-        AttendanceSheet sheet = systemController.queryAttendance(date);
-        System.out.println();
-        System.out.println("-------Attendance Mode : " + date + "-------");
-
-
-        int parsedUid;
-        String line = "";
-        while (true) {
-            try {
-                System.out.println("Enter UID (or q to exit):");
-                line = stdin.nextLine();
-                parsedUid = ParseUtility.parseUID(line);
-                sheet.markPresent(parsedUid);
-            } catch (RuntimeException e) {
-                if (line.equals("q")) return;
-
-                System.out.print("Enter valid uid: ");
-                line = stdin.nextLine();
-            }
-        }
-
+        return systemController.queryAttendance(date);
     }
+
+    private boolean hasAttendances() {
+        System.out.println();
+        if (attendanceLists.isEmpty()) {
+            System.out.println("There are no registered attendances.");
+            return false;
+        }
+        return true;
+    }
+
+
 }
