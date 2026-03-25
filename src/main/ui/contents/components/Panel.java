9b26bf0de6bbed1618a18752959003d461b45b48
i0bz <andreiancheta101@gmail.com>
2026-03-25 17:34:19 +0800

    feat: added dynamic padding size for subclasses to use

--- src/main/ui/contents/components/Panel.java
+++ src/main/ui/contents/components/Panel.java
@@ -6,16 +6,36 @@ import javax.swing.border.CompoundBorder;
 import com.formdev.flatlaf.ui.FlatEmptyBorder;
 import com.formdev.flatlaf.ui.FlatLineBorder;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 
 public class Panel {
-    protected JPanel mainPanel = new JPanel(new GridBagLayout());
+    protected GridBagLayout layout = new GridBagLayout();
+    protected JPanel mainPanel = new JPanel(layout);
     protected GridBagConstraints constraints = new GridBagConstraints();
+
+
+    private int paddingSize = 20;
 
     protected Color borderColor = Color.decode("#cecfd1");
     protected FlatLineBorder line_border = new FlatLineBorder(new Insets(0,0,0,0), borderColor, 1, 20);
-    protected FlatEmptyBorder padding = new FlatEmptyBorder(20,20,20,20);
+    protected FlatEmptyBorder padding = new FlatEmptyBorder(paddingSize,paddingSize,paddingSize,paddingSize);
     protected CompoundBorder border = new CompoundBorder(line_border, padding);
+
+
+
+    protected void dynamicPadding() {
+        mainPanel.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                paddingSize = (int) Math.min(25, e.getComponent().getWidth() * 0.04);
+                padding = new FlatEmptyBorder(paddingSize,paddingSize,paddingSize,paddingSize);
+                border = new CompoundBorder(line_border, padding);
+                mainPanel.setBorder(border);
+            }
+        });
+    }
 
     protected Panel() {
         mainPanel.setBorder(border);
