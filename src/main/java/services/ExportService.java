b084950bbf16fa65095adc426332ea3a126afc8e
i0bz <andreiancheta101@gmail.com>
2026-04-12 16:11:01 +0800

    feat [import/export]: added importing and exporting services

---
+++ src/main/java/services/ExportService.java
@@ -0,0 +1,108 @@
+package services;
+
+import entity.Student;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ExportService {
+    private final StudentService studentManagement;
+    private final AttendanceService attendanceService;
+
+
+    public ExportService(StudentService studentManagement, AttendanceService attendanceService) {
+        this.studentManagement = studentManagement;
+        this.attendanceService = attendanceService;
+    }
+
+    public void exportExcel(File file) {
+        try (Workbook workbook = new XSSFWorkbook()) {
+            writeStudentsExcel(workbook);
+            writeEventsExcel(workbook);
+            writeAttendancesExcel(workbook);
+
+
+            try (FileOutputStream outputFile = new FileOutputStream(file)) {
+                workbook.write(outputFile);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeStudentsExcel(Workbook workbook) {
+        Sheet sheet = workbook.createSheet("Students List");
+
+        Row header = sheet.createRow(0);
+        header.createCell(0).setCellValue("ID");
+        header.createCell(1).setCellValue("Name");
+
+        Map<Integer, Student> students = studentManagement.getAllStudentsByID();
+        AtomicInteger iterator = new AtomicInteger(1);
+
+        students.forEach((id, student) -> {
+            Row row = sheet.createRow(iterator.get());
+            row.createCell(0).setCellValue(id);
+            row.createCell(1).setCellValue(student.name());
+            iterator.getAndIncrement();
+        });
+    }
+
+    private void writeEventsExcel(Workbook workbook) {
+        Sheet eventsSheet = workbook.createSheet("Events List");
+
+        Map<String, LocalDate> events = attendanceService.queryEvents();
+
+        Row header = eventsSheet.createRow(0);
+        header.createCell(0).setCellValue("Event Name");
+        header.createCell(0).setCellValue("Date");
+
+        AtomicInteger iterator = new AtomicInteger(1);
+
+        events.forEach((name, date) -> {
+            Row row = eventsSheet.createRow(iterator.get());
+            row.createCell(0).setCellValue(name);
+            row.createCell(1).setCellValue(date);
+            iterator.getAndIncrement();
+        });
+    }
+
+    private void writeAttendancesExcel(Workbook workbook) {
+        Sheet attendancesSheet = workbook.createSheet("Attendance Sheet");
+        Row header = attendancesSheet.createRow(0);
+        List<String> events = attendanceService.queryEvents().keySet().stream().toList();
+        List<Integer> students = studentManagement.getAllStudentsByID().keySet().stream().toList();
+
+        int iterator = 1;
+
+        for (String event : events) {
+            header.createCell(iterator++).setCellValue(event);
+        }
+
+        iterator = 1;
+        for (Integer id : students) {
+            Row row = attendancesSheet.createRow(iterator);
+            row.createCell(iterator++).setCellValue(id);
+        }
+
+
+        int studentIterator = 1;
+        int eventIterator = 1;
+        for (Integer id : students) {
+            Row row = attendancesSheet.getRow(studentIterator++);
+            for (String event : events) {
+                Cell cell = row.createCell(eventIterator++);
+                if (attendanceService.isPresent(event, id)) cell.setCellValue("Present");
+                else cell.setCellValue("Absent");
+            }
+        }
+    }
+
+}
---
+++ src/main/java/services/ImportService.java
@@ -0,0 +1,68 @@
+package services;
+
+import org.apache.poi.ss.usermodel.*;
+import utility.ParseUtility;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ImportService {
+    private final StudentService studentManagement;
+    private final AttendanceService attendanceService;
+    private final DataFormatter dataFormatter = new DataFormatter();
+
+    public ImportService(StudentService studentManagement, AttendanceService attendanceService) {
+        this.studentManagement = studentManagement;
+        this.attendanceService = attendanceService;
+    }
+
+    public void importStudentsExcel(File file) throws IOException {
+        Workbook workbook = WorkbookFactory.create(file);
+        Sheet studentSheet = workbook.getSheet("Students List");
+
+        for (Row row : studentSheet) {
+            if (row.equals(studentSheet.getRow(0))) continue;
+            String name = dataFormatter.formatCellValue(row.getCell(1));
+            String uid = dataFormatter.formatCellValue(row.getCell(0));
+            studentManagement.enroll(name, ParseUtility.parseUID(uid));
+        }
+    }
+
+    public void importEventsExcel(File file) throws IOException {
+        Workbook workbook = WorkbookFactory.create(file);
+        Sheet attendanceSheet = workbook.getSheet("Events List");
+
+        for (Row row : attendanceSheet) {
+            String event = dataFormatter.formatCellValue(row.getCell(0));
+            String date = dataFormatter.formatCellValue(row.getCell(1));
+            attendanceService.createAttendance(event, ParseUtility.parseDate(date));
+        }
+    }
+
+    public void importAttendancesExcel(File file) throws IOException {
+        importStudentsExcel(file);
+        importEventsExcel(file);
+
+        Workbook workbook = WorkbookFactory.create(file);
+        Sheet attendanceSheet = workbook.getSheet("Attendance Sheets");
+        Row headerRow = attendanceSheet.getRow(0);
+
+        int i = 0;
+
+        for (Row row : attendanceSheet) {
+            if (row.equals(headerRow)) continue;
+            Cell uidCell = row.getCell(0);
+            int uid = ParseUtility.parseUID(dataFormatter.formatCellValue(uidCell));
+
+            for (Cell cell : row) {
+                i++;
+                if (cell.equals(uidCell)) continue;
+                if (dataFormatter.formatCellValue(cell).equals("Present")) {
+                    String event = dataFormatter.formatCellValue(headerRow.getCell(i));
+                    attendanceService.markPresent(event, uid);
+                }
+            }
+        }
+
+    }
+}
